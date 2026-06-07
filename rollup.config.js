1a8af8a4e83c988f2346dffdafa56e4f9df15c1a
Yao <kirayao@hotmail.com>
2026-06-08 00:25:21 +0800

    build: 更新@rollup/plugin-terser并优化rollup配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,23 @@
 import terser from '@rollup/plugin-terser';
+import { readFileSync } from 'fs';
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default {
   input: 'src/main.js',
   output: {
     file: 'src/main.min.js',
     format: 'es',
-    sourcemap: true
+    sourcemap: true,
+    banner: `/* ever-cache v${pkg.version} */`
   },
   plugins: [
-    terser()
+    terser({
+      format: {
+        comments: function(node, comment) {
+          return comment.value.includes('ever-cache');
+        }
+      }
+    })
   ]
 };
